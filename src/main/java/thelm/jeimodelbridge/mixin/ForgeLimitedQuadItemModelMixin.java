b678434a1f5b42c603b7a22810a5798118c633d4
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-06-02 04:05:31 +0800

    1.20.1

---
+++ src/main/java/thelm/jeimodelbridge/mixin/ForgeLimitedQuadItemModelMixin.java
@@ -0,0 +1,39 @@
+package thelm.jeimodelbridge.mixin;
+
+import java.util.function.Supplier;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import mezz.jei.forge.platform.ForgeLimitedQuadItemModel;
+import net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel;
+import net.fabricmc.fabric.api.renderer.v1.render.RenderContext;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.client.model.BakedModelWrapper;
+
+@Mixin(ForgeLimitedQuadItemModel.class)
+public abstract class ForgeLimitedQuadItemModelMixin extends BakedModelWrapper<BakedModel> implements FabricBakedModel {
+
+	private ForgeLimitedQuadItemModelMixin(BakedModel originalModel) {
+		super(originalModel);
+	}
+
+	@Override
+	public boolean isVanillaAdapter() {
+		return ((FabricBakedModel)originalModel).isVanillaAdapter();
+	}
+
+	@Override
+	public void emitBlockQuads(BlockAndTintGetter level, BlockState state, BlockPos pos, Supplier<RandomSource> randomSupplier, RenderContext context) {
+		((FabricBakedModel)originalModel).emitBlockQuads(level, state, pos, randomSupplier, context);
+	}
+
+	@Override
+	public void emitItemQuads(ItemStack stack, Supplier<RandomSource> randomSupplier, RenderContext context) {
+		((FabricBakedModel)originalModel).emitItemQuads(stack, randomSupplier, context);
+	}
+}
